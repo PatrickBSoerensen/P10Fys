be191ed8a82c85bb6b386a5b2d0c0183a7f3518d
PatrickBSoerensen <patrick@linderuphus.com>
2018-02-22 14:33:00 +0100

    Segments -> Points
    SegmentsLine og SegmentsCirc har fået nyt navn der dækker bedre hvad de er.
    Derudover er enhedsvektorene skrevet ind i Antenna

--- Antenna.m
+++ Antenna.m
@@ -1,8 +1,9 @@
 classdef Antenna
     properties
         Length;
-        SegmentsLine;
-        SegmentsCircle;
+        PointsLine;
+        PointsCircle;
+        Segments;
         Radii;
         Centre;
         Lin;
@@ -13,22 +14,26 @@ classdef Antenna
         CircTopTest;
         Coord;
         CoordTest;
+        zHat;
+        tHat;
+        gamma;
     end
     
     methods
-        function ant = Antenna(length, segmentsline, segmentscircle, radii, centre)
+        function ant = Antenna(length, pointsline, pointscircle, radii, centre)
             %Setting up parameters
             ant.Length = length;
-            ant.SegmentsLine = segmentsline;
-            ant.SegmentsCircle = segmentscircle;
+            ant.PointsLine = pointsline;
+            ant.PointsCircle = pointscircle;
             ant.Radii = radii;
             ant.Centre = centre;
+            ant.Segments = 2*pointscircle + pointsline-2;
             %Splitting cylinder part of antenna
             cylinderlen = length-2.*radii;
-            Lin = linspace(-cylinderlen./2, cylinderlen./2, segmentsline);
-            ant.Lin = Lin(2:segmentsline-1);
-            seglen = cylinderlen/segmentsline;
-            ant.LinTest = linspace(-cylinderlen./2+seglen./2, cylinderlen./2-seglen./2, segmentsline-1);
+            Lin = linspace(-cylinderlen./2, cylinderlen./2, pointsline);
+            ant.Lin = Lin(2:pointsline-1);
+            seglen = cylinderlen/pointsline;
+            ant.LinTest = linspace(-cylinderlen./2+seglen./2, cylinderlen./2-seglen./2, pointsline-1);
             %Splitting lower circ part
             ant.CircBot = CreateCirc(ant, 1, 0);
             ant.CircBotTest = CreateCirc(ant, 1, 1);
@@ -38,26 +43,55 @@ classdef Antenna
             %Creating full coordinate matrix
             ant.Coord = CreateCoord(ant, 0);
             ant.CoordTest = CreateCoord(ant, 1);
+            [ant.tHat, ant.zHat, ant.gamma] = UnitVecs(ant);
+        end
+        
+        function [tHat, zHat, gamma] = UnitVecs(ant)
+        %Unit vectors should be part of Antenna class
+        tHat(1:ant.PointsCircle,1) ... 
+        = -ant.Radii.*sin(linspace(-pi/2, 0, ant.PointsCircle));
+        tHat(ant.PointsCircle+1:ant.PointsCircle+ant.PointsLine-2,1) ... 
+        = 0;
+        tHat(ant.PointsCircle+ant.PointsLine-1: ... 
+        2*ant.PointsCircle+ant.PointsLine-2,1)...
+        = -ant.Radii.*sin(linspace(0, pi/2, ant.PointsCircle));%x coord
+        
+        tHat(:,2) = 0;%y coord
+        tHat(:,3) = 1;%z coord
+        tHat(1:ant.PointsCircle,3) = ... 
+        ant.Radii.*cos(linspace(-pi/2, 0, ant.PointsCircle));%z coord
+        tHat(ant.PointsCircle+ant.PointsLine-1: ... 
+        2*ant.PointsCircle+ant.PointsLine-2,3)...
+        = ant.Radii.*cos(linspace(0, pi/2, ant.PointsCircle));%z coord
+    
+        tHat = tHat./sqrt(tHat(:,1).^2+tHat(:,2).^2+tHat(:,3).^2);%normalizing to unit
+
+        zHat = tHat; %For dimensions
+        zHat(:,1) = 0; %x coord
+        zHat(:,2) = 0; %y coord
+        zHat(:,3) = 1; %z coord
+        
+        gamma = acos(dot(tHat,zHat,2));
         end
         
         function circ = CreateCirc(ant, bot, test)
-            arclength = pi/(2*ant.SegmentsCircle);
+            arclength = pi/(2*ant.PointsCircle);
             circ = [];
             if test
                 if bot
-                    dist = linspace(-pi/2+arclength/2, 0-arclength/2, ant.SegmentsCircle-1);
+                    dist = linspace(-pi/2+arclength/2, 0-arclength/2, ant.PointsCircle-1);
                     circ(:,2) = (-ant.Length/2)+ant.Radii+ant.Radii*sin(dist);
                 else
-                    dist = linspace(0+arclength/2, pi/2-arclength/2, ant.SegmentsCircle-1);
+                    dist = linspace(0+arclength/2, pi/2-arclength/2, ant.PointsCircle-1);
                     circ(:,2) = (ant.Length/2)-ant.Radii+ant.Radii*sin(dist);
                 end
                 circ(:,1) = ant.Radii*cos(dist);
             else
                 if bot
-                    dist = linspace(-pi/2, 0, ant.SegmentsCircle);
+                    dist = linspace(-pi/2, 0, ant.PointsCircle);
                     circ(:,2) = (-ant.Length/2)+ant.Radii+ant.Radii*sin(dist);
                 else
-                    dist = linspace(0, pi/2, ant.SegmentsCircle);
+                    dist = linspace(0, pi/2, ant.PointsCircle);
                     circ(:,2) = (ant.Length/2)-ant.Radii+ant.Radii*sin(dist);
                 end
                 circ(:,1) = ant.Radii*cos(dist);
@@ -67,27 +101,27 @@ classdef Antenna
         function coord = CreateCoord(ant, test)
            coord = [];
            if test
-               segcirc = ant.SegmentsCircle-1;
+               segcirc = ant.PointsCircle-1;
                circbot = ant.CircBotTest;
                circtop = ant.CircTopTest;
                lin = ant.LinTest;
            else
-               segcirc = ant.SegmentsCircle;
+               segcirc = ant.PointsCircle;
                circbot = ant.CircBot;
                circtop = ant.CircTop;
                lin = ant.Lin;
            end
            coord(1:segcirc,1) = circbot(:,2);
            coord(1:segcirc,2) = circbot(:,1);%Should be expanded with centre
-           coord(1:segcirc,3) = pi/(2*ant.SegmentsCircle)*ant.Radii;
+           coord(1:segcirc,3) = pi/(2*ant.PointsCircle)*ant.Radii;
            
-           coord(segcirc+1:segcirc+ant.SegmentsLine-2+test,1) = lin;
-           coord(segcirc+1:segcirc+ant.SegmentsLine-2+test,2) = ant.Radii;%Should be expanded with centre
-           coord(segcirc+test:segcirc+ant.SegmentsLine-2+test,3) = (ant.Length-2.*ant.Radii)/ant.SegmentsLine;
+           coord(segcirc+1:segcirc+ant.PointsLine-2+test,1) = lin;
+           coord(segcirc+1:segcirc+ant.PointsLine-2+test,2) = ant.Radii;%Should be expanded with centre
+           coord(segcirc+test:segcirc+ant.PointsLine-2+test,3) = (ant.Length-2.*ant.Radii)/ant.PointsLine;
           
-           coord(segcirc+ant.SegmentsLine-1+test:2*segcirc+ant.SegmentsLine-2+test,1) = circtop(:,2);
-           coord(segcirc+ant.SegmentsLine-1+test:2*segcirc+ant.SegmentsLine-2+test,2) = circtop(:,1);%Should be expanded with centre
-           coord(segcirc+ant.SegmentsLine-1+test:2*segcirc+ant.SegmentsLine-2+test,3) = pi/(2*ant.SegmentsCircle)*ant.Radii;%Should be expanded with centre
+           coord(segcirc+ant.PointsLine-1+test:2*segcirc+ant.PointsLine-2+test,1) = circtop(:,2);
+           coord(segcirc+ant.PointsLine-1+test:2*segcirc+ant.PointsLine-2+test,2) = circtop(:,1);%Should be expanded with centre
+           coord(segcirc+ant.PointsLine-1+test:2*segcirc+ant.PointsLine-2+test,3) = pi/(2*ant.PointsCircle)*ant.Radii;%Should be expanded with centre
         end
         
         function green = Green(ant, k)
