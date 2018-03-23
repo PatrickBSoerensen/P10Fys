60e4ac0dac800efd60aad0dbd27bf4a4791179fc
PatrickBSoerensen <patrick@linderuphus.com>
2018-03-23 13:26:26 +0100

    things and stuff

--- LoadTesting.m
+++ LoadTesting.m
@@ -11,32 +11,101 @@
 %Load the binary stl file
 stl = stlread('AntBinMesh.stl');
 %% faces and unique vertices
-faces=zeros(size(stl.faces));
+UV = zeros(size(stl.vertices));
+faces=stl.faces;
+count = 1;
 for i=1:length(stl.vertices)
     a = stl.vertices==stl.vertices(i,:);
     a = sum(a,2);
     b = a==3;
     k=find(b);
-    for j=1:length(k)
-        faces(stl.faces(:,:) == k(j)) = k(1);
-    end
-    if all(faces)
-        break
+    UV(k(1),:) = stl.vertices(k(1),:);
+    for j=2:length(k)
+        c=stl.faces == k(j);
+        faces(c) = k(1);
     end
 end
-UV=stl.vertices(faces);
-%% Gibson connectivity list
 for i=1:length(UV)
-    a = faces == i;
+    UV(i,4) = i;
 end
-%% Coupling matrix
-% N=length(UV);
-% gmat=zeros(N,N);
-% for i=1:N
-%     xi=p(1,i); yi=p(2,i);
-%     for j=1:i
-%         xj=p(1,j); yj=p(2,j);
-%         r_ij=sqrt((xi-xj)^2+(yi-yj)^2);
-%         gmat(i,j)=1i/4*besselh(0,1,k0*n1*r_ij)*k0^2*(eps2-eps1);
-%     end
-% end
+k = find(~any(UV(:,1:3),2));
+UV( ~any(UV(:,1:3),2), : ) = []; 
+for i=1:length(UV)
+    faces(faces==UV(i,4)) = i;
+end
+UV(:,4) = []; 
+
+%% Gibson connectivity list
+ConnectCell = cell(length(UV),2);
+for i=1:length(UV)
+    a = faces==i;
+    b = sum(a,2);
+    b = find(b);
+    a(~any(a,2),:) = logical(1-a(~any(a,2),:));
+    a = logical(1-a);
+    
+    Connected = unique(faces(a));
+    Connected = Connected(Connected>i);
+    ConnectCell{i,1} = Connected;
+    
+    ConnectCell{i,2} = unique(b);
+end
+
+EdgeNumber = 1;
+
+for i=1:length(UV)
+    temp = faces(ConnectCell{i,2},:);
+    nodes = ConnectCell{i,1};
+    temp = sort(temp,2);
+    for j=1:length(nodes)
+        a = temp(:,2:end) == nodes(j);
+        b = sum(a,2);
+        b = find(b);
+        NodesOfInterrest = temp(b,:);
+        c = NodesOfInterrest ~= i;
+        d = logical(NodesOfInterrest ~= nodes(j));
+        c = logical(c.*d);
+        
+        Triangles = temp(b,:);
+        EdgePoints = [i,nodes(j)];
+        NotEdgePoints = Triangles(c);
+        
+        L = abs(sqrt(sum((UV(EdgePoints(1),:)-UV(EdgePoints(2),:)).^2)));
+        
+        LforAP = abs(sqrt(sum((UV(EdgePoints(1),:)-UV(NotEdgePoints(1),:)).^2)));
+        LforAM = abs(sqrt(sum((UV(EdgePoints(1),:)-UV(NotEdgePoints(2),:)).^2)));
+        
+        AP = (L*LforAP)/2;
+        AM = (L*LforAM)/2;
+        
+        %Central point for a cluster
+        EdgeList(EdgeNumber, 1) = i;
+        %Connection point
+        EdgeList(EdgeNumber, 2) = nodes(j);
+        %Plus triangle point
+        EdgeList(EdgeNumber, 3) = NotEdgePoints(1);
+        %Minus triangle point
+        EdgeList(EdgeNumber, 4) = NotEdgePoints(2);
+        
+        %Plus
+        Basis{EdgeNumber,1} = @(r) (NotEdgePoints(1,:)-r) * L/(2*AP);
+        %Minus
+        Basis{EdgeNumber,2} = @(r) (NotEdgePoints(1,:)-r) * L/(2*AM);
+        %Plus
+        BasisDeriv(EdgeNumber,1) = -L/AP;
+        %Minus
+        BasisDeriv(EdgeNumber,2) = L/AM;
+        
+        EdgeNumber = EdgeNumber + 1;
+    end
+end
+%% MoM?
+    Z = zeros(EdgeNumber, EdgeNumber);
+    b = 1:EdgeNumber;
+    g = @(r) exp(1i*k*r)/(4*pi*r);
+    
+    for i=1:EdgeNumber
+        
+        Z(i,j) = integral()+integral();
+        
+    end
